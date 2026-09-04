f7f56c51e892e0ff60d96c04ab4aad478a309177
kobaltgit <vash_email@example.com>
2026-09-04 18:55:33 +0300

    fix(website): update release constants and download links to v2.1.0

--- website/lib/constants.dart
+++ website/lib/constants.dart
@@ -1,13 +1,13 @@
 class AppConstants {
   static const String repoUrl = 'https://github.com/kobaltgit/Undoit';
-  static const String releaseTag = 'v2.0.0';
-  static const String releaseUrl = 'https://github.com/kobaltgit/Undoit/releases/tag/v2.0.0';
+  static const String releaseTag = 'v2.1.0';
+  static const String releaseUrl = 'https://github.com/kobaltgit/Undoit/releases/tag/v2.1.0';
   static const String setupDownloadUrl =
-      'https://github.com/kobaltgit/Undoit/releases/download/v2.0.0/Undoit_2.0.0_x64-setup.exe';
+      'https://github.com/kobaltgit/Undoit/releases/download/v2.1.0/Undoit_2.1.0_x64-setup.exe';
   static const String portableDownloadUrl =
-      'https://github.com/kobaltgit/Undoit/releases/download/v2.0.0/Undoit_2.0.0_x64_portable.exe';
+      'https://github.com/kobaltgit/Undoit/releases/download/v2.1.0/Undoit_2.1.0_x64_portable.exe';
   static const String msiDownloadUrl =
-      'https://github.com/kobaltgit/Undoit/releases/download/v2.0.0/Undoit_2.0.0_x64_en-US.msi';
+      'https://github.com/kobaltgit/Undoit/releases/download/v2.1.0/Undoit_2.1.0_x64_en-US.msi';
   static const String issuesUrl = 'https://github.com/kobaltgit/Undoit/issues';
   static const String licenseUrl = 'https://github.com/kobaltgit/Undoit/blob/main/LICENSE';
 }
